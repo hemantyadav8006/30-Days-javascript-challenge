4ebe9e62671cae9b11e960c984ca5e2ee14fcc70
Hemant Kumar Yadav <55074926+hemantyadav8006@users.noreply.github.com>
2024-07-17 00:30:27 +0530

    updated Day3.js

--- Day3.js
+++ Day3.js
@@ -6,24 +6,24 @@ Activity 1: If-Else Statements
 • Task 2: Write a program to check if a person is eligible to vote (age >= 18) and log the result to the console.
 */
 
-// let num = 2;
-// if (num > 0) {
-//     console.log(`${num} is Positive`);
-// }
-// else if (num < 0) {
-//     console.log(`${num} is Negative`);
-// }
-// else {
-//     console.log(`${num} is zero`);
-// }
+let number1 = 2;
+if (number1 > 0) {
+    console.log(`${number1} is Positive`);
+}
+else if (number1 < 0) {
+    console.log(`${number1} is Negative`);
+}
+else {
+    console.log(`${number1} is zero`);
+}
 
-// let personName = "Hemant";
-// let personAge = 23;
-// if (personAge >= 18) {
-//     console.log(`${personName} is eligible to vote`);
-// } else {
-//     console.log(`${personName} is not eligible to vote`);
-// }
+let personName = "Hemant";
+let personAge = 23;
+if (personAge >= 18) {
+    console.log(`${personName} is eligible to vote`);
+} else {
+    console.log(`${personName} is not eligible to vote`);
+}
 
 
 /*
@@ -31,16 +31,16 @@ Activity 2: Nested If-Else Statements
 Task 3: Write a program to find the largest of three numbers using nested if-else statements.
 */
 
-// let num1 = 5;
-// let num2 = 12;
-// let num3 = 32;
-// if (num1 > num2 && num1 > num3) {
-//     console.log(`num1: ${num1} is greater than num2: ${num2} and num3: ${num3}`);
-// } else if (num2 > num1 && num2 > num3) {
-//     console.log(`num2: ${num2} is greater than num1: ${num1} and num3: ${num3}`);
-// } else {
-//     console.log(`num3: ${num3} is greater than num1: ${num1} and num2: ${num2}`);
-// }
+let num1 = 5;
+let num2 = 12;
+let num3 = 32;
+if (num1 > num2 && num1 > num3) {
+    console.log(`num1: ${num1} is greater than num2: ${num2} and num3: ${num3}`);
+} else if (num2 > num1 && num2 > num3) {
+    console.log(`num2: ${num2} is greater than num1: ${num1} and num3: ${num3}`);
+} else {
+    console.log(`num3: ${num3} is greater than num1: ${num1} and num2: ${num2}`);
+}
 
 /*
 Activity 3: Switch Case
@@ -48,64 +48,64 @@ Activity 3: Switch Case
 • Task 5: Write a program that uses a switch case to assign a grade ('A', 'B', 'C', 'D', 'F') based on a score and log the grade to the console.
 */
 // Task4
-// let numDay = 4;
-// switch (numDay) {
-//     case 1:
-//         console.log("Monday");
-//         break;
-//     case 2:
-//         console.log("Tuesday");
-//         break;
-//     case 3:
-//         console.log("Wednesday");
-//         break;
-//     case 4:
-//         console.log("Thursday");
-//         break;
-//     case 5:
-//         console.log("Friday");
-//         break;
-//     case 6:
-//         console.log("Saturday");
-//         break;
-//     case 7:
-//         console.log("Sunday");
-//         break;
+let numDay = 4;
+switch (numDay) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
 
-//     default:
-//         console.log("INVALID NUMBER!");
-//         break;
-// }
+    default:
+        console.log("INVALID NUMBER!");
+        break;
+}
 
 // Task 5
-// let studentScore = 10;
-// switch (studentScore) {
-//     case 10:
-//         console.log("Student grade is : A!");
-//         break;
-//     case 9:
-//         console.log("Student grade is : A!");
-//         break;
-//     case 8:
-//         console.log("Student grade is : B!");
-//         break;
-//     case 7:
-//         console.log("Student grade is : B!");
-//         break;
-//     case 6:
-//         console.log("Student grade is : C!");
-//         break;
-//     case 5:
-//         console.log("Student grade is : C!");
-//         break;
-//     case 4:
-//         console.log("Student grade is : D!");
-//         break;
+let studentScore = 10;
+switch (studentScore) {
+    case 10:
+        console.log("Student grade is : A!");
+        break;
+    case 9:
+        console.log("Student grade is : A!");
+        break;
+    case 8:
+        console.log("Student grade is : B!");
+        break;
+    case 7:
+        console.log("Student grade is : B!");
+        break;
+    case 6:
+        console.log("Student grade is : C!");
+        break;
+    case 5:
+        console.log("Student grade is : C!");
+        break;
+    case 4:
+        console.log("Student grade is : D!");
+        break;
 
-//     default:
-//         console.log("FAILED!");
-//         break;
-// }
+    default:
+        console.log("FAILED!");
+        break;
+}
 
 /*
 Activity 4: Conditional (Ternary) Operator
@@ -113,16 +113,22 @@ Activity 4: Conditional (Ternary) Operator
 */
 // task 6
 
-// let num = 30;
-// let result = num % 2 == 0 ? `${num} is Even!` : `${num} is Odd!`;
-// console.log(result);
+let num = 30;
+let result = num % 2 == 0 ? `${num} is Even!` : `${num} is Odd!`;
+console.log(result);
 
 /*
 Activity 5: Combining Conditions
 Task 7: Write a program to check if a year is a leap year using multiple conditions (divisible by 4, but not 100 unless also divisible by 400) and log the result to the console. 
 */
 
-
+//task 7
+const year = 2014;
+if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    console.log(`${year} Leap year!`);
+} else {
+    console.log(`${year} non leap year!`);
+}
 
 /*
 Feature Request:
