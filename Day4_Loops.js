cbc58bc12eb3ed4ca8a5de4671fa8ae31e8a8229
Hemant Kumar Yadav <55074926+hemantyadav8006@users.noreply.github.com>
2024-07-17 21:40:59 +0530

    Day4 challenge complete!

--- Day4_Loops.js
+++ Day4_Loops.js
@@ -1,5 +1,5 @@
-/* 
-Day 4: Loops 
+/*
+Day 4: Loops
 
 Tasks/Activities:
 
@@ -8,14 +8,14 @@ Activity 1: For Loop
 • Task 2: Write a program to print the multiplication table of 5 using a for loop.
 */
 // task 1
-// for (let i = 1; i <= 10; i++) {
-//     console.log(i);
-// }
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
 
 // task 2
-// for (let i = 1; i <= 10; i++) {
-//     console.log(`5 X ${i} = ${5 * i}`);
-// }
+for (let i = 1; i <= 10; i++) {
+    console.log(`5 X ${i} = ${5 * i}`);
+}
 
 /*
 Activity 2: While Loop
@@ -24,20 +24,20 @@ Activity 2: While Loop
 */
 
 // task 3
-// let sum = 0;
-// let i = 1;
-// while (i <= 10) {
-//     sum = sum + i;
-//     i++;
-// }
-// console.log(`Sum is : ${sum}`);
+let sum = 0;
+let r = 1;
+while (r <= 10) {
+    sum = sum + r;
+    r++;
+}
+console.log(`Sum is : ${sum}`);
 
 // task 4
-// let i = 10;
-// while (i >= 1) {
-//     console.log(i);
-//     i--;
-// }
+let j = 10;
+while (j >= 1) {
+    console.log(j);
+    j--;
+}
 
 /*
 Activity 3: Do...While Loop
@@ -46,24 +46,24 @@ Activity 3: Do...While Loop
 */
 
 // task 5
-// let i = 1;
-// do {
-//     console.log(i);
-//     i++;
-// } while (i <= 5);
+let f = 1;
+do {
+    console.log(f);
+    f++;
+} while (f <= 5);
 
 // task 6
-// let fact = 1;
-// let i = 0;
-// do {
-//     if (i === 0) {
-//         break;
-//     } else {
-//         fact = fact * i;
-//         i--;
-//     }
-// } while (i >= 1);
-// console.log(`Factorial is : ${fact}`);
+let fact = 1;
+let i = 0;
+do {
+    if (i === 0) {
+        break;
+    } else {
+        fact = fact * i;
+        i--;
+    }
+} while (i >= 1);
+console.log(`Factorial is : ${fact}`);
 
 
 /*
@@ -78,25 +78,44 @@ Task 7: Write a program to print a pattern using nested for loops:
 */
 
 // task 7
-let i = 1;
-let str = "";
-while (i <= 5) {
+let t = 1;
+while (t <= 5) {
+    let str = "";
     let j = 1;
-    while (j <= i) {
-        console.log(str + "*");
+    while (j <= t) {
+        str = str + "* ";
         j++;
     }
-    console.log(str + "\n");
-    i++;
-
+    t++;
+    console.log(str);
 }
+
 
 /*
 Activity 5: Loop Control Statements
 • Task 8: Write a program to print numbers from 1 to 10, but skip the number 5 using the continue statement.
 • Task 9: Write a program to print numbers from 1 to 10, but stop the loop when the number is 7 using the break statement.
+*/
+
+// task 8
+for (let i = 1; i <= 10; i++) {
+    if (i == 5) {
+        continue;
+    }
+    console.log(i);
+}
+
+// task 9
+for (let i = 0; i <= 10; i++) {
+    if (i == 7) {
+        break;
+    }
+    console.log(i);
+}
 
 
+
+/*
 Feature Request:
 1. Number Printing Script: Write a script that prints numbers from 1 to 10 using a for loop and a while loop.
 2. Multiplication Table Script: Create a script that prints the multiplication table of 5 using a for loop.
